3b88b56d540b0645048ec0b55884a7a256e6dabd
Clayton <claytonwhitten63@gmail.com>
2023-07-11 15:20:04 -0400

    finish the calculation screen

--- main.py
+++ main.py
@@ -62,7 +62,8 @@ def syna_screen():
     )
     syna_text.insert("0.0", "The Staggered Yeast Nutrient Addition is a formula"
                      " that calculates how much yeast nutrient should be added at"
-                     " specific time intervals throughout the fermentation process.")
+                     " specific time intervals throughout the fermentation process."
+                     " All calculations are done assuming you use one 5 gram packet of yeast per gallon.")
     syna_text.tag_config("center", justify='center')
     syna_text.configure(state="disabled", font=main_font, text_color='white', height=50, width=800, fg_color='transparent')
     syna_text.pack(padx=20)
@@ -116,7 +117,7 @@ def syna_screen():
     starting_gravity_entry.pack(pady=(0, 10))
 
     fruit_gravity_text = customtkinter.CTkTextbox(app)
-    fruit_gravity_text.insert("0.0", "Gravity From Fruit (Optional):")
+    fruit_gravity_text.insert("0.0", "Fruit Gravity (Optional):")
     fruit_gravity_text.tag_config("center", justify='center')
     fruit_gravity_text.configure(state="disabled", font=("Aldrich", 12), text_color='white', height=10, fg_color='transparent')
     fruit_gravity_text.pack(pady=(10, 0))
@@ -125,7 +126,35 @@ def syna_screen():
         app,
         placeholder_text='Enter Fruit Gravity:'
     )
-    fruit_gravity_entry.pack()
+    fruit_gravity_entry.pack(pady=(0, 20))
+
+    calculate_button = customtkinter.CTkButton(
+        app,
+        text='Calculate SYNA',
+        font=main_font,
+        command=lambda: show_answer(syna_calculation(yeast_selection.get(), batch_unit_selection.get(),
+                                         batch_size=batch_size_entry.get(), starting_gravity=starting_gravity_entry.get(),
+                                         fruit_gravity=fruit_gravity_entry.get())),
+        corner_radius=10,
+        fg_color="#0065FF"
+    )
+    calculate_button.pack()
+
+    # function to show the answer
+def show_answer(syna_calc):
+    answer_level = customtkinter.CTkToplevel(app)
+    answer_level.iconbitmap('custom_assets/bottle.ico')
+    answer_level.title("Staggered Yeast Nutrient Addition Calculation")
+    answer_level.geometry("600x400")
+    answer_level.attributes('-topmost', 'true')
+    calculation_text = customtkinter.CTkLabel(
+        answer_level,
+        text=f'Add {syna_calc} grams of Fermaid-O at 24, 48, and 72 hours, and then at the 7 day mark',
+        font=customtkinter.CTkFont(family='Aldrich', size=18),
+        text_color='#0065FF',
+        wraplength=200
+    )
+    calculation_text.pack()
 
 
 def main_screen():
@@ -161,10 +190,41 @@ def to_brix(specific_gravity):
     rounded_brix = round(brix, 2)
     return rounded_brix
 
+# finds nitrogen requirement of yeast
+def n_requirement(yeast):
+    match yeast:
+        case 'Lalvin EC-1118':
+            return 0.15
+        case 'Lalvin 71B':
+            return 0.15
+        case 'Lalvin K1-V1116':
+            return 0.15
+        case 'Lalvin QA23':
+            return 0.15
+        case 'Lalvin D-47':
+            return 0.15
+        case _:
+            return -1
+
 
 # Staggered Yeast Nutrient Addition
-def syna_calculation(yeast_type, unit_of_measure, batch_size, starting_gravity, fruit_gravity=None):
-    pass
+def syna_calculation(yeast_type, unit_of_measure, batch_size, starting_gravity, fruit_gravity=''):
+    new_fruit = 0
+    starting_gravity = float(starting_gravity)
+    if fruit_gravity != 0 and fruit_gravity.isdigit():
+        new_fruit = float(fruit_gravity)
+        new_fruit = new_fruit - 1
+    elif fruit_gravity.isalnum():
+        new_fruit = 0
+    brix = to_brix(starting_gravity + new_fruit)
+    nitrogen_demand = n_requirement(yeast_type)
+    if nitrogen_demand == -1:
+        return "Please select a yeast"
+    if unit_of_measure == 'Liters':
+        batch_size = round((batch_size * 0.264), 2)
+    grams_fermaid_o = brix * nitrogen_demand * float(batch_size)
+    nutrient_addition = round((grams_fermaid_o / 4.0), 1)
+    return nutrient_addition
 
 # End Of Functions
 
@@ -175,6 +235,7 @@ app = customtkinter.CTk()
 customtkinter.set_default_color_theme("dark-blue")
 customtkinter.FontManager.load_font("custom_assets/Aldrich-Regular.ttf")
 main_font = customtkinter.CTkFont(family="Aldrich", size=14)
+
 
 app.geometry('800x600')
 app.iconbitmap('custom_assets/bottle.ico')
